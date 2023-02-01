f11a779f0a2eb64adac4ba052355137c496c6914
ImadDabbura <imad.dabbura@hotmail.com>
2023-02-01 11:49:37 -0600

    feat: Add callback base class

---
+++ dl_utils/callbacks.py
@@ -0,0 +1,39 @@
+import re
+
+
+class Callback:
+    """Base class for all callbacks."""
+
+    _order = 0
+
+    def set_learner(self, learner):
+        self.learner = learner
+
+    def __getattr__(self, k):
+        return getattr(self.learner, k)
+
+    @property
+    def name(self):
+        """
+        Returns the name of the callback after removing the word `callback`
+        and then convert it to snake (split words by underscores).
+        """
+        name = re.sub(r"Callback$", "", self.__class__.__name__)
+        return Callback.camel2snake(name or "callback")
+
+    def __call__(self, cb_name):
+        f = getattr(self, cb_name, None)
+        if f and f():
+            return True
+        return False
+
+    @staticmethod
+    def camel2snake(name):
+        """
+        Convert name of callback by inserting underscores between small and capital
+        letters. For example, `TestCallback` becomes `test_callback`.
+        """
+        pattern1 = re.compile("(.)([A-Z][a-z]+)")
+        pattern2 = re.compile("([a-z0-9])([A-Z])")
+        name = re.sub(pattern1, r"\1_\2", name)
+        return re.sub(pattern2, r"\1_\2", name).lower()
