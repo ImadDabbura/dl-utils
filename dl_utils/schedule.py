ffea138a5ca5dfc0d157fbb2b8328238e2bfbb3e
ImadDabbura <imad.dabbura@hotmail.com>
2023-02-02 10:51:01 -0600

    feat: Add common schedulers

--- dl_utils/schedule.py
+++ dl_utils/schedule.py
@@ -13,3 +13,35 @@ def annealer(func):
         return partial(func, *args, **kwargs)
 
     return annealer_wrapper
+
+
+@annealer
+def no_sched(start, end, pos):
+    """Constant schedular."""
+    return start
+
+
+@annealer
+def lin_sched(start, end, pos):
+    """Linear scheduler."""
+    return start + (end - start) * pos
+
+
+@annealer
+def cos_sched(start, end, pos):
+    """Cosine scheduler."""
+    return start + (1 + math.cos(math.pi * (1 - pos))) * (end - start) / 2
+
+
+@annealer
+def exp_sched(start, end, pos):
+    """Exponential scheduler."""
+    return start * (end / start) ** pos
+
+
+def cos_1cycle_anneal(start, high, end):
+    """
+    combine two cosine schedulers where first scheduler goes from `start` to
+    `high` and second scheduler goes from `high` to `end`.
+    """
+    return [cos_sched(start, high), cos_sched(high, end)]
