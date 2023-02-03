65ce1531f06fb36a7e3c434a48db2c50abc1f01c
ImadDabbura <imad.dabbura@hotmail.com>
2023-02-03 08:21:13 -0600

    feat: Add DataBunch

--- dl_utils/data.py
+++ dl_utils/data.py
@@ -25,3 +25,27 @@ def get_dls(train_ds, valid_ds, bs, **kwargs):
         DataLoader(train_ds, batch_size=bs, shuffle=True, **kwargs),
         DataLoader(valid_ds, batch_size=bs * 2, shuffle=False, **kwargs),
     )
+
+
+class DataBunch:
+    """
+    Utility class that stores train and valid dataloaders as well as number of
+    input features and number of output (1 for regression, number of classes
+    for classfication).
+    """
+
+    def __init__(
+        self, train_dl: DataLoader, valid_dl: DataLoader, c_in: int, c_out: int
+    ):
+        self.train_dl = train_dl
+        self.valid_dl = valid_dl
+        self.c_in = c_in
+        self.c_out = c_out
+
+    @property
+    def train_ds(self):
+        return self.train_dl.dataset
+
+    @property
+    def valid_ds(self):
+        return self.valid.dataset
