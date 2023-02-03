8705ee5acdd7209d0e9bf8767ab45d2da3b6557a
ImadDabbura <imad.dabbura@hotmail.com>
2023-02-03 08:17:47 -0600

    feat: Add get_dls
    This function is very helpful because we always need train and valid
    DataLoaders for any training task.

--- dl_utils/data.py
+++ dl_utils/data.py
@@ -14,3 +14,14 @@ def compose(x, funcs, *args, order="_order", **kwargs):
     for func in sorted(listify(funcs), key=lambda x: getattr(x, order, 0)):
         x = func(x, *args, **kwargs)
     return x
+
+
+def get_dls(train_ds, valid_ds, bs, **kwargs):
+    """
+    Returns two dataloaders: 1 for training and 1 for 1 for validation. The
+    validation dataloader has twice the batch size and doesn't shuffle data.
+    """
+    return (
+        DataLoader(train_ds, batch_size=bs, shuffle=True, **kwargs),
+        DataLoader(valid_ds, batch_size=bs * 2, shuffle=False, **kwargs),
+    )
