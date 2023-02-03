3fede1d12ea40ea54746f4901696d31df8bdbe6e
ImadDabbura <imad.dabbura@hotmail.com>
2023-02-03 08:42:16 -0600

    feat: Add text list

---
+++ dl_utils/text/data.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from ..data import ItemList, get_files
+
+
+def read_file(fn):
+    with open(fn, encoding="utf8") as f:
+        return f.read()
+
+
+class TextList(ItemList):
+    @classmethod
+    def from_files(
+        cls, path, extensions=".txt", recurse=True, include=None, **kwargs
+    ):
+        """
+        Build an text list from list of files in the `path` end with
+        extensions, optionally recursively.
+        """
+        return cls(
+            get_files(path, extensions, recurse, include), path, **kwargs
+        )
+
+    def get(self, i):
+        """Returns text in the file as string if `i` is path to a file."""
+        if isinstance(i, Path):
+            return read_file(i)
+        return i
